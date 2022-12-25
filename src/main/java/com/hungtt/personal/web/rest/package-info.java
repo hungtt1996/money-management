f8a1c6be85728d4bacfe80a6d3e0274ee450a3eb
hungtt <hung>
2022-12-25 20:01:34 +0700

    Initial version of money_management generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/hungtt/personal/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.hungtt.personal.web.rest;
---
+++ src/main/java/com/hungtt/personal/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.hungtt.personal.web.rest.vm;
